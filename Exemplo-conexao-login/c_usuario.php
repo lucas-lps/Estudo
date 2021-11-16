0a9e0b73de27d7dd680980b7d7b2a8dacdbb1f96
Lucas Lopes <lucaslpsmacedo@gmail.com>
2021-11-16 12:30:26 -0300

    Adicionado o arquivo c_usuario.php

--- Exemplo-conexao-login/c_usuario.php
+++ Exemplo-conexao-login/c_usuario.php
@@ -6,9 +6,9 @@
             $conexao = new Conexao;
             $select = mysqli_query($conexao->conectar(), "SELECT * FROM funcionario WHERE usuario = '$usuario' AND senha = '$senha'");
             if(mysqli_num_rows($select) > 0) {
-
+                echo "<script language='javascript' type='text/javascript'>alert('Login efetuado com sucesso!');window.location.href='index.html';</script>"; die();
             } else {
-                
+                echo "<script language='javascript' type='text/javascript'>alert('Usuário ou senha incorreto(s)');window.location.href='index.html';</script>"; die();
             }
         }
     }
